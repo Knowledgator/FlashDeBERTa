394604d22a4d34fd9c8a4e65e503a0c2137901ac
Nitin Surya <nitinsurya1991@gmail.com>
2025-09-04 16:23:18 +0530

    GLiNER#263 Fix get_attention_mask function that allows for batched inference.

--- src/flashdeberta/model.py
+++ src/flashdeberta/model.py
@@ -301,6 +301,12 @@ class FlashDebertaV2Encoder(DebertaV2Encoder):
         self.gradient_checkpointing = False
 
     def get_attention_mask(self, attention_mask):
+        if attention_mask.dim() <= 2:
+            extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+            attention_mask = extended_attention_mask * extended_attention_mask.squeeze(-2).unsqueeze(-1)
+        elif attention_mask.dim() == 3:
+            attention_mask = attention_mask.unsqueeze(1)
+
         return attention_mask
 
 class FlashDebertaV2PreTrainedModel(PreTrainedModel):
